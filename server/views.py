ac68800bdfb0e51789e86b1f5ab4295f060c8ba0
Prabhakar Kafle <pkafleprabhakar@gmail.com>
2022-01-10 09:37:57 +0545

    added requirements and removed ryan function calls

--- server/views.py
+++ server/views.py
@@ -4,83 +4,32 @@ from server.settings import UPLOAD_FOLDER, OUTPUT_FOLDER
 from flask import request, Response, render_template
 import json
 
-from scripts import checkbox_detect, table_analysis, util, template_extract
-from utils import CustomEncoder
+# from scripts import checkbox_detect, table_analysis, util, template_extract
+# from utils import CustomEncoder
 
 output_fpath = '/static/outputs/'
 
 @app.route('/')
 def home():
+    """
+    URL: /
+    Homepage for the app
+    """
     return render_template('index.html')
 
 @app.route('/process', methods=['POST'])
 def process():
-    action = request.form['actionType']
-    page = int(request.form['page'])
-    print('page number: ', page)
-    document = request.files['document']
-    filename = UPLOAD_FOLDER + document.filename
-    name = os.path.basename(filename).split('.')[0]
-    document.save(filename)
-    print("The filename: ", filename)
-    im_paths = util.pdf_to_image(filename)
-    image_path = im_paths[page-1]
+    """
+    URL: /process
+    Returns JSON response for the request sent to process.
+    """
+    # action = request.form['actionType']
+    # page = int(request.form['page'])
+    # document = request.files['document']
+    # filename = UPLOAD_FOLDER + document.filename
+    # name = os.path.basename(filename).split('.')[0]
+    # document.save(filename)
 
-    if action == 'checkbox':
-        clusters, img = checkbox_detect.checkbox_detect(image_path, plot=False, fileout=OUTPUT_FOLDER + name)
-        clusters = [{'type': 'checkboxes', 'data': cluster} for cluster in clusters]
-        image_path = output_fpath + os.path.basename(img)
-        response = {
-            'clusters': clusters,
-            'image': image_path
-        }
-    elif action == 'uniform_table':
-        img_fname = name + "_uniform.jpg"
-        result = table_analysis.extract_tables(image_path, out_img=OUTPUT_FOLDER + img_fname) #check for uniform table
-        # result = table_analysis.return_table(image_path, outfile=OUTPUT_FOLDER + img_fname) #check for uniform table
-        # csv_fname = name + "_uniform.csv"
-        # template_fname = name + "_uniform.json"
-        # if len(result) > 0:
-        #     data = table_analysis.read_tables(image_path, result[0], result[1], fpath=OUTPUT_FOLDER, #+ 'table/',
-        #                                                                                  csv_name=csv_fname, template_name=template_fname)
-        #     response = [{
-        #         'type': 'uniform_table',
-        #         'data': data
-        #     }]
-        # else:
-        #     response = []
 
-        response = []
-        for table in result:
-            if len(table) > 0:
-                response.append({
-                    'type': 'uniform_table',
-                    'data': table
-                })
-        response = {
-            'clusters': response,
-            'image': output_fpath + img_fname
-        }
-    elif action == 'non_uniform_table':
-        img_fname = name + "_non_uniform.jpg"
-        response = table_analysis.get_horizontal_lines(image_path, outfile=OUTPUT_FOLDER + img_fname)
-        clusters = []
-        for cluster in response:
-            clusters.append({
-                'type': 'non_uniform_table',
-                'data': cluster
-            })
-        response = {
-            'clusters': clusters,
-            'image': output_fpath + img_fname
-        }
-    else:
-        template = request.files['template']
-        template_filename = UPLOAD_FOLDER + template.filename
-        template.save(template_filename)
-        
-        name = os.path.basename(filename).split('.')[0]
-        output_file = output_fpath + name + '.json'
-        response = template_extract.extract_template(image_path, filename, template_filename, output_fpath, output_file)
-
-    return json.dumps(response, cls=CustomEncoder)
+    # return json.dumps(response, cls=CustomEncoder)
+    return
